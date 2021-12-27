6dedd902a926c47a8f603a6702d678802e708902
davistr <davistr1216@gmail.com>
2021-12-26 21:35:50 -0500

    added timer and option to save score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-// Create variables to access page elements
+// create variables to access page elements
+var playerScore = 0;
+// create timer elements
+var timerEl = document.getElementById('timer');
+var timeRemaining = 60;
 
 var startQuizBtn = document.getElementById("start-quiz");
 var questionText = document.getElementById("question-area");
 var answerText = document.getElementById("answer-area");
-
 // create button elements
 var btn1 = document.createElement("button");
 var btn2 = document.createElement("button");
@@ -14,7 +17,7 @@ btn1.className = "answer-btn";
 btn2.className = "answer-btn";
 btn3.className = "answer-btn";
 btn4.className = "answer-btn";
-
+// set total score
 var totalScore = 0;
 
 // Create array to hold quiz questions
@@ -27,7 +30,7 @@ var questions = [
             c: "1993",
             d: "1998"
         },
-        correctAnswer: "c"
+        correctAnswer: "1993"
     },
     {
         question: "Which tag is used to create a button?",
@@ -37,7 +40,7 @@ var questions = [
             c: "<input type = 'button'>",
             d: "none of the above"
         },
-        correctAnswer: "b"
+        correctAnswer: "<button>"
     },
     {
         question: "Which tag is used to link javascript code?",
@@ -47,21 +50,35 @@ var questions = [
             c: "<javascr>",
             d: "<script>"
         },
-        correctAnswer: "d"
+        correctAnswer: "<script>"
+    },
+    {
+        question: "What does CSS stand for?",
+        answers: {
+            a: "Center Style Sheets",
+            b: "Cascading Style Sheets",
+            c: "Cascading Style Set",
+            d: "Cascading Sheet Styling"
+        },
+        correctAnswer: "Cascading Style Sheets"
+    },
+    {
+        question: "Which of the following ways would you comment code in JavaScript?",
+        answers: {
+            a: "<!-- -->",
+            b: "/* */",
+            c: "//",
+            d: "all of the above"
+        },
+        correctAnswer: "//"
     }
 
 ];
-
+// run start quiz function on start button click
 startQuizBtn.onclick = function () {
-
     startQuiz();
-
+    setTimer();
 };
-
-// var startQuiz = function () {
-
-//     startQuizBtn.classList.add("hide");
-// };
 
 var startQuiz = function () {
 
@@ -84,48 +101,236 @@ var startQuiz = function () {
 
     btn1.onclick = function () {
         window.alert("Incorrect!");
-        resetQuestion();
+        reduceTime();
+        q2();
     };
 
+    btn2.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q2();
+    };
+
+    btn3.onclick = function () {
+        window.alert("Correct!");
+        playerScore += 20;
+        q2();
+    };
+
+    btn4.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q2();
+    };
 };
 
-var resetQuestion = function () {
+var q2 = function () {
 
-    //console.log("yayyyy");
-    questionText.innerHTML = "";
-    answerText.innerHTML = "";
+    // hide start quiz button
+    startQuizBtn.style.display = "none";
 
+    questionText.innerHTML = questions[1].question;
 
-    for (i = 1; i < questions.length; i++) {
+    btn1.textContent = "a: " + questions[1].answers.a;
+    answerText.appendChild(btn1);
 
-        questionText.innerHTML = questions[i].question;
+    btn2.textContent = "b: " + questions[1].answers.b;
+    answerText.appendChild(btn2);
 
-        btn1.textContent = "a:" + questions[i].answers.a;
-        answerText.appendChild(btn1);
+    btn3.textContent = "c: " + questions[1].answers.c;
+    answerText.appendChild(btn3);
 
-        btn2.textContent = "b:" + questions[i].answers.b;
-        answerText.appendChild(btn2);
-
-        btn3.textContent = "c:" + questions[i].answers.c;
-        answerText.appendChild(btn3);
-
-        btn4.textContent = "d:" + questions[i].answers.d;
-        answerText.appendChild(btn4);
-
-    }
+    btn4.textContent = "d: " + questions[1].answers.d;
+    answerText.appendChild(btn4);
 
     btn1.onclick = function () {
         window.alert("Incorrect!");
-        resetQuestion();
+        reduceTime();
+        q3();
     };
 
-    //resetQuestion();
-};
+    btn2.onclick = function () {
+        window.alert("Correct!");
+        playerScore += 20;
+        q3();
+    };
+
+    btn3.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q3();
+    };
+
+    btn4.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q3();
+    };
+}
+
+var q3 = function () {
+    startQuizBtn.style.display = "none";
+
+    questionText.innerHTML = questions[2].question;
+
+    btn1.textContent = "a: " + questions[2].answers.a;
+    answerText.appendChild(btn1);
+
+    btn2.textContent = "b: " + questions[2].answers.b;
+    answerText.appendChild(btn2);
+
+    btn3.textContent = "c: " + questions[2].answers.c;
+    answerText.appendChild(btn3);
+
+    btn4.textContent = "d: " + questions[2].answers.d;
+    answerText.appendChild(btn4);
+
+    btn1.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q4();
+    };
+
+    btn2.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q4();
+    };
+
+    btn3.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q4();
+    };
+
+    btn4.onclick = function () {
+        window.alert("Correct!");
+        playerScore += 20;
+        q4();
+    };
+}
+
+var q4 = function () {
+    startQuizBtn.style.display = "none";
+
+    questionText.innerHTML = questions[3].question;
+
+    btn1.textContent = "a: " + questions[3].answers.a;
+    answerText.appendChild(btn1);
+
+    btn2.textContent = "b: " + questions[3].answers.b;
+    answerText.appendChild(btn2);
+
+    btn3.textContent = "c: " + questions[3].answers.c;
+    answerText.appendChild(btn3);
+
+    btn4.textContent = "d: " + questions[3].answers.d;
+    answerText.appendChild(btn4);
+
+    btn1.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q5();
+    };
+
+    btn2.onclick = function () {
+        window.alert("Correct!");
+        playerScore += 20;
+        q5();
+    };
+
+    btn3.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q5();
+    };
+
+    btn4.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        q5();
+    };
+}
+
+var q5 = function () {
+    startQuizBtn.style.display = "none";
+
+    questionText.innerHTML = questions[4].question;
+
+    btn1.textContent = "a: " + questions[4].answers.a;
+    answerText.appendChild(btn1);
+
+    btn2.textContent = "b: " + questions[4].answers.b;
+    answerText.appendChild(btn2);
+
+    btn3.textContent = "c: " + questions[4].answers.c;
+    answerText.appendChild(btn3);
+
+    btn4.textContent = "d: " + questions[4].answers.d;
+    answerText.appendChild(btn4);
+
+    btn1.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        quizEnd();
+    };
+
+    btn2.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        quizEnd();
+    };
+
+    btn3.onclick = function () {
+        window.alert("Correct!");
+        playerScore += 20;
+        quizEnd();
+    };
+
+    btn4.onclick = function () {
+        window.alert("Incorrect!");
+        reduceTime();
+        quizEnd();
+    };
+}
+
+var quizEnd = function () {
+    // create input field for user initials
+    var scoreInput = document.createElement("input");
+    scoreInput.setAttribute("type", "text");
+    // create button to submit score
+    var scoreBtn = document.createElement("button");
+    scoreBtn.textContent = "Submit";
+    scoreBtn.className = "answer-btn";
+
+    questionText.innerHTML = "Finished!" + "<br>" + "Your Score: " + playerScore + "%";
+
+    answerText.innerHTML = "Please enter your initials: ";
+    //answerText.appendChild(scoreDiv);
+    answerText.appendChild(scoreInput);
+    answerText.appendChild(scoreBtn);
+
+}
 
 var setTimer = function () {
+    time = setInterval(function () {
+        if (timeRemaining > 1) {
+            timerEl.textContent = "Time: " + timeRemaining;
+            timeRemaining--;
+        }
+        else {
+            timerEl.textContent = 0;
+            quizEnd();
+        }
+    }, 1000);
 
 };
 
+var reduceTime = function () {
 
+    if (timeRemaining >= 10) {
+        timeRemaining -= 10;
+    }
+};
 
 
